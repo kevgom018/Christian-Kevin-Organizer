0ba6a86158f761a7e501a31404a432b80ac7b8e7
kevgom018 <157429350+kevgom018@users.noreply.github.com>
2024-10-14 22:02:56 -0400

    testing model to detect whether there is an event prior to its analysis

--- organizer.py
+++ organizer.py
@@ -6,12 +6,13 @@ from transformers import pipeline
 from huggingface_hub import InferenceClient
 
 # Test input for Hugging Face
+isThereEvent = "There is an event being announced."
 eventQ = "What is the event?"
 hostQ = "Who hosts the event?"
 dayQ = "What day is the event"
 timeQ = "At what time will the event occur?"
-message = "PandaHat Society invites you to the 18th Hackathon thursday at 11am."
-
+message = "PandaHat Society invites you to the 18th Hackathon thursday at 11am." # Event should be detected
+noEvent = "There will be no chemistry test this sunday at 6:00pm." # No event should be detected
 # This class represents the user and uses their info to access email information
 # TODO: Add necessary login info for all services to be searched
 # TODO: Add the functionality for reading emails, going through moodle, displaying with tkinter (or alternatives) and correctly process task complexity
@@ -70,12 +71,18 @@ def testSummarizer():
     file = open("APIToken.txt", "r")
     token = file.readline()
     inference = InferenceClient(token = token)
-    event = inference.question_answering(question = eventQ, context = message)
-    host = inference.question_answering(question = hostQ, context = message)
-    day = inference.question_answering(question = dayQ, context = message)
-    time = inference.question_answering(question = timeQ, context = message)
-    eventSummary = event.answer + " on " + day.answer + " at " + time.answer + " by " + host.answer
-    print(eventSummary)
+    classifier = pipeline("text-classification", model = "roberta-large-mnli")
+    eventRecognized = classifier(message + " " + isThereEvent)
+    if eventRecognized[0]["label"] == "ENTAILMENT":
+        print("Event detected.\nSummary:")
+        event = inference.question_answering(question = eventQ, context = message)
+        host = inference.question_answering(question = hostQ, context = message)
+        day = inference.question_answering(question = dayQ, context = message)
+        time = inference.question_answering(question = timeQ, context = message)
+        eventSummary = event.answer + " on " + day.answer + " at " + time.answer + " by " + host.answer
+        print(eventSummary)
+    else:
+        print("No event detected.")
 
 # **Here we will put together all the segments of our code and later possibly move to different files
 def main():
